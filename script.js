ed455329d301c80a3f4067972109fcdad08e7097
susi <yuanliu755@gmail.com>
2024-04-08 08:32:00 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+video.addEventListener("click", function video() {
+  alert("Video Traileer The NUN 1");
+});
+
+audio.addEventListener("click", function audio() {
+  alert("Audio Trailer The NUN 1");
+});
+
+gambar.addEventListener("click", function gambar() {
+  alert(" Gambar Poster");
+});
